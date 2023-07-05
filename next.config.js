3d24cdafb1321b7f43f309ec124fc5e3a07ac72a
SoniaaSonia <sonia.eminence@gmail.com>
2023-07-05 19:02:56 +0530

    done

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     FRONTEND_PORT: "3000",
     WEB_API_URL: "http://localhost:5000",
     // WEB_APP_URL: "http://localhost:3000",
-    IPDATA_KEY:"ddd2af4694eac7e519fbadc4d166cbf83441f1de5f5184b5bdaaa228"
+    IPDATA_KEY: "9f41d6f4795852feac8ab106aede039f0485748978ed1749ced3ac4d",
   },
 };
 
