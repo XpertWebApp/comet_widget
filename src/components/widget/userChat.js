57de65fce9687e8ee4fde4f0f710cc21250e95a6
Samay Nandan <samay.emimence@gmail.com>
2023-07-21 21:30:47 +0530

    changes in design

--- src/components/widget/userChat.js
+++ src/components/widget/userChat.js
@@ -1,91 +1,98 @@
-import { ChatUser } from '@/assets/icon'
-import moment from 'moment'
-import { Typewriter } from 'react-simple-typewriter'
-import WelComeMessage from './welcomeMessage'
+import { ChatUser } from "@/assets/icon";
+import moment from "moment";
+import { Typewriter } from "react-simple-typewriter";
+import WelComeMessage from "./welcomeMessage";
 import ChatBoatIcon from "../../assets/img/chatboat-icon.png";
-import Image from 'next/image';
-const UserChat = ({ val, projectData, setNewMessage ,withAgentSatus }) => {
+import Image from "next/image";
+const UserChat = ({ val, projectData, setNewMessage, withAgentSatus }) => {
   return (
     <>
-      {val?.sender == 'member' ||
-      val?.sender == 'bot' ||
-      val?.sender == 'admin' ? (
+      {val?.sender == "member" ||
+      val?.sender == "bot" ||
+      val?.sender == "admin" ? (
         <>
-          <li className={val?.type == 'message' ? 'reply' : 'request'}>
-            <div className='chat-reply-field'>
-            <div
-              className="chat-field"
-              style={{
-                borderColor: projectData?.text_container,
-              }}
-            >
-              <p style={{ color: projectData?.text_color }}>
-                {val._id ? (
-                  <>{val?.message}</>
-                ) : (
-                  <Typewriter
-                    words={[val?.message]}
-                    typeSpeed={3}
-                    delaySpeed={5}
-                  />
+          <li className={val?.type == "message" ? "reply" : "request"}>
+            <div className="chat-reply-field">
+              <div
+                className="chat-field"
+                style={{
+                  borderColor: projectData?.text_container,
+                }}
+              >
+                <p style={{ color: projectData?.text_color }}>
+                  {val._id ? (
+                    <>{val?.message}</>
+                  ) : (
+                    <Typewriter
+                      words={[val?.message]}
+                      typeSpeed={3}
+                      delaySpeed={5}
+                    />
+                  )}
+                </p>
+                {val?.type == "message" && (
+                  <span className="time">
+                    {moment(val.createdAt).format("HH:mm A")}
+                  </span>
                 )}
-              </p>
-              {val?.type == 'message' && (
-                <span className="time">
-                  {moment(val.createdAt).format('HH:mm A')}
+              </div>
+              {val?.sender == "bot" ? (
+                <span className="user-icon">
+                  <Image src={ChatBoatIcon} alt="icon" />
                 </span>
+              ) : (val?.sender == "admin" || val?.sender == "member") &&
+                val.type == "message" ? (
+                <span className="user-icon">
+                  <ChatUser />
+                </span>
+              ) : (
+                ""
               )}
             </div>
-            <span className="user-icon">
-                <Image src={ChatBoatIcon} alt='icon'/>
-            </span>
-                </div>
           </li>
         </>
       ) : (
-        ''
+        ""
       )}
-      {val?.sender == 'user' ? (
+      {val?.sender == "user" ? (
         <li className="sender">
-           <div className='chat-user-field'>
-           <span className="user-icon">
+          <div className="chat-user-field">
+            <span className="user-icon">
               <ChatUser />
             </span>
-          <div
-            className="chat-field"
-            style={{
-              borderColor: projectData?.user_text_container,
-            }}
-          >
-           
-          
-            <div className="chating">
-              <p
-                style={{
-                  color: projectData?.text_color,
-                }}
-              >
-                {val._id ? (
-                  <>{val?.message}</>
-                ) : (
-                  <Typewriter
-                    words={[val?.message]}
-                    typeSpeed={3}
-                    delaySpeed={5}
-                  />
-                )}
-              </p>
-              <span className="time">
-                {moment(val.createdAt).format('HH:mm A')}
-              </span>
-            </div>
+            <div
+              className="chat-field"
+              style={{
+                borderColor: projectData?.user_text_container,
+              }}
+            >
+              <div className="chating">
+                <p
+                  style={{
+                    color: projectData?.text_color,
+                  }}
+                >
+                  {val._id ? (
+                    <>{val?.message}</>
+                  ) : (
+                    <Typewriter
+                      words={[val?.message]}
+                      typeSpeed={3}
+                      delaySpeed={5}
+                    />
+                  )}
+                </p>
+                <span className="time">
+                  {moment(val.createdAt).format("HH:mm A")}
+                </span>
+              </div>
             </div>
           </div>
         </li>
       ) : (
-        ''
+        ""
       )}
     </>
-  )
-}
-export default UserChat
+  );
+};
+export default UserChat;
